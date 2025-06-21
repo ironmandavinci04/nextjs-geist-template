98c6a58d85cca02f9012b60b0593539f8ebab591
ironmandavinci04 <ironmandavinci@gmail.com>
2025-06-21 05:59:18 -0700

    Add fix_path.sh 1750510758792

---
+++ fix_path.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script to fix PATH environment variable to include /bin and /usr/bin
+
+export PATH=/bin:/usr/bin:$PATH
+echo "PATH environment variable fixed to include /bin and /usr/bin"
